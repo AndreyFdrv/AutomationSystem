7086ce3db52239ca905429c4f4c5799163e35cde
Leonardo\Leo <sacreder>
2017-02-22 20:46:38 +0300

    b1 new

--- AutomationStructure/Automation.Model/Modules/KitchenUpModule/KitchenUpViewUtility.cs
+++ AutomationStructure/Automation.Model/Modules/KitchenUpModule/KitchenUpViewUtility.cs
@@ -54,6 +54,7 @@ namespace Automation.Model.Modules.KitchenUpModule
                 "Что это такое"
             };
             return column;
+            int stop;
         }
 
         private DataGridViewComboBoxColumn GetFacadeMaterial()
